179613a1978763cea5be07f4067157e089fdac10
Wangkang0512 <wangkang0512@buaa.edu.cn>
2025-05-22 22:25:38 +0800

    AI服务化

--- src/main/java/com/wk/aiagent/app/CounselingApp.java
+++ src/main/java/com/wk/aiagent/app/CounselingApp.java
@@ -13,6 +13,7 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -60,6 +61,13 @@ public class CounselingApp {
                 .build();
     }
 
+
+    /**
+     * AI 基础对话（支持多轮对话记忆）
+     * @param message
+     * @param chatId
+     * @return
+     */
     public String doChat(String message, String chatId){
         ChatResponse response = chatClient.prompt()
                 .user(message)
@@ -72,7 +80,30 @@ public class CounselingApp {
         return content;
     }
 
+    /**
+     * AI 基础对话（支持多轮对话记忆，SSE 流式传输）
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public Flux<String> doChatByStream(String message, String chatId) {
+        return chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .stream()
+                .content();
+    }
 
+
+    /**
+     * psychologyAppRagCloudAdvisor
+     * @param message
+     * @param chatId
+     * @return
+     */
     public PsychologyReport doChatWithReport(String message, String chatId) {
         PsychologyReport psychologyReport = chatClient
                 .prompt()
@@ -108,6 +139,12 @@ public class CounselingApp {
     @Resource
     private ToolCallback[] allTools;
 
+    /**
+     * doChatWithTools
+     * @param message
+     * @param chatId
+     * @return
+     */
     public String doChatWithTools(String message, String chatId) {
         ChatResponse response = chatClient
                 .prompt()
@@ -123,6 +160,7 @@ public class CounselingApp {
 //        log.info("content: {}", content);
         return content;
     }
+
 
 
 }
